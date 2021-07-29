2c7d746950b71db4101597aa6251ba54d4a39a12
cdiadmin <abelmagana88920@gmail.com>
2021-07-29 09:16:12 +0800

    change route logo as router link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,25 +8,25 @@ import {
   useColorModeValue,
   Stack,
   Container,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-import { sections } from 'utils/config';
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { sections } from "utils/config";
 
-import { ReactComponent as Logo } from 'assets/img/logo.svg';
-import {Link as RouterLink} from 'react-router-dom'
+import { ReactComponent as Logo } from "assets/img/logo.svg";
+import { Link as RouterLink } from "react-router-dom";
 
 const NavLink = ({ link, children }) => (
   <Link
     as={RouterLink}
     px={2}
     py={1}
-    rounded={'md'}
+    rounded={"md"}
     _active={{
-      color: 'brand.500',
+      color: "brand.500",
     }}
     _hover={{
-      textDecoration: 'none',
-      color: useColorModeValue('dark.100'),
+      textDecoration: "none",
+      color: useColorModeValue("dark.100"),
     }}
     color="brand.100"
     to={link}
@@ -42,18 +42,18 @@ function Navigation() {
     <Container maxW="12xl" px={[4, 8, 16]}>
       <Box>
         <Flex h={16} alignItems="center" justifyContent="space-between">
-          <Box as={Link} to="/">
+          <Box as={RouterLink} to="/">
             <Logo />
           </Box>
           <IconButton
-            size={'md'}
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label="Open Menu"
-            display={{ md: 'none' }}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
           {/* nav items */}
-          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+          <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
             {sections.map((item) => (
               <NavLink link={item.link} key={item.name}>
                 {item.name}
@@ -63,7 +63,7 @@ function Navigation() {
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
+          <Box pb={4} display={{ md: "none" }}>
             <Stack as="nav" spacing={4}>
               {sections.map((item) => (
                 <NavLink link={item.link} key={item.name}>
