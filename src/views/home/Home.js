e4afbcf9bc0e15fa24754e1fb8548f0adbc3b48a
icyjms <icyjms@yahoo.com>
2021-07-29 08:33:45 +0800

    Resolve responsive design in Partner componenet

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -6,16 +6,16 @@ import {
   Text,
   Button,
   Container,
-} from '@chakra-ui/react';
-
-import { ReactComponent as DeviceImg } from 'assets/img/pre-launch-device.svg';
-import GetNotified from 'components/GetNotified';
-import LodiText from 'components/LodiText';
-import Section from 'components/Section';
-import { Link } from 'react-router-dom';
+} from "@chakra-ui/react";
+// import { ReactComponent as LodiLogo } from "assets/img/logo.svg";
+import { ReactComponent as DeviceImg } from "assets/img/pre-launch-device.svg";
+import GetNotified from "components/GetNotified";
+import LodiText from "components/LodiText";
+import Section from "components/Section";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const subtext = 'Get notified when we launch';
+  const subtext = "Get notified when we launch";
 
   const goTo = (path) => {
     // window.history.pushState({}, document.title, path);
@@ -23,13 +23,13 @@ function Home() {
   };
 
   return (
-    <Section>
+    <Section minHeight="70vh">
       <Container maxW="12xl" px={[4, 8, 16]}>
         <Stack
           justifyContent="space-between"
           spacing={{ base: 16, md: 10 }}
           py={{ base: 4, md: 16 }}
-          direction={{ base: 'column', md: 'row' }}
+          direction={{ base: "column", md: "row" }}
         >
           <Stack>
             <Stack
@@ -41,12 +41,12 @@ function Home() {
               <Heading
                 lineHeight={1.1}
                 fontWeight={700}
-                fontSize={{ base: '4xl', sm: '3xl', lg: '7xl' }}
+                fontSize={{ base: "4xl", sm: "3xl", lg: "7xl" }}
               >
                 <Text
                   as="span"
                   color="dark.100"
-                  style={{ textTransform: 'uppercase' }}
+                  style={{ textTransform: "uppercase" }}
                 >
                   Parating na ang bagong
                   <LodiText />
@@ -56,7 +56,7 @@ function Home() {
               <Stack>
                 <Text
                   color="gray.500"
-                  fontSize={{ base: '1xl', sm: '2xl', lg: '3xl' }}
+                  fontSize={{ base: "1xl", sm: "2xl", lg: "3xl" }}
                 >
                   {subtext}
                   {/* Gusto mo bang maging LODI sa negosyo?
@@ -66,35 +66,39 @@ function Home() {
               </Stack>
             </Stack>
             <Stack
-              direction={{ base: 'column', sm: 'row' }}
+              direction={{ base: "column", sm: "row" }}
               spacing={[4, 8, 12]}
             >
               <Stack direction="column">
                 <Text color="gray.500">Ikaw ba ay may negosyo?</Text>
-                <Button bg="brand.100" onClick={() => goTo('#partner')}>
-                  BE A LODI PARTNER
+                <Button bg="brand.100" onClick={() => goTo("#partner")}>
+                  MAGING LODI PARTNER
                 </Button>
               </Stack>
               <Stack direction="column">
                 <Text color="gray.500">Ikaw ba ay Rider?</Text>
-                <Button as={Link} to="/rider">
-                  BE A LODI RIDER
+                <Button as={Link} to="/rider" backgroundColor="cyan">
+                  MAGING LODI RIDER
                 </Button>
               </Stack>
             </Stack>
           </Stack>
-          <Flex
-            display={{ base: 'none', sm: 'block' }}
-            flex={1}
-            justify="center"
-            align="center"
-            position="relative"
-            px={[4, 8, 12]}
-          >
-            <Box position="relative" width="full" overflow="hidden">
-              <DeviceImg />
-            </Box>
-          </Flex>
+          {
+            <Flex
+              display={{ base: "none", sm: "block" }}
+              flex={1}
+              justify="center"
+              align="center"
+              position="relative"
+              // px={[4, 8, 12]}
+            >
+              {
+                <Box position="relative" width="full" overflow="hidden">
+                  <DeviceImg />
+                </Box>
+              }
+            </Flex>
+          }
         </Stack>
       </Container>
     </Section>
--- src/views/partner/Partner.js
+++ src/views/partner/Partner.js
@@ -9,64 +9,72 @@ import {
   HStack,
   Icon,
   VStack,
-} from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons';
+  Center,
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 
-import LodiText from 'components/LodiText';
-import { offers } from 'utils/config';
-
-import { ReactComponent as PartnerImg } from 'assets/img/partner.svg';
-import GetNotified from 'components/GetNotified';
-import Section from 'components/Section';
+import LodiText from "components/LodiText";
+import { offers } from "utils/config";
+// import { ReactComponent as LodiLogo } from "assets/img/logo.svg";
+import { ReactComponent as PartnerImg } from "assets/img/partner.svg";
+import GetNotified from "components/GetNotified";
+import Section from "components/Section";
 
 function Partner() {
   return (
     <Section id="partner">
       <Container maxW="12xl" px={[4, 8, 16]}>
-        <Box mb={{ base: 8, md: 20 }}>
-          <Heading
-            color="dark.100"
-            mb={5}
-            fontSize={{ base: '4xl', md: '6xl' }}
-          >
-            BE A
-            <LodiText />
-            PARTNER
-          </Heading>
-          <Text fontSize="xl" color="gray.400">
-            Give your restaurant and online business a boost
-          </Text>
-        </Box>
-        <Box py="4">
-          <Text>
-            We offer the best way for businesses such as yours to tap into a big
-            market online!
-          </Text>
-        </Box>
-        <Stack direction={{ base: 'column', lg: 'row' }}>
+        <Stack direction={{ base: "column", lg: "row" }}>
           <Flex direction="column" justifyContent="space-evenly">
-            <SimpleGrid
-              templateRows="repeat(4, 1fr)"
+            <Box mb={{ base: 8, md: 20 }}>
+              <Heading
+                color="dark.100"
+                mb={5}
+                fontSize={{ base: "4xl", md: "6xl" }}
+              >
+                BE A
+                <LodiText />
+                PARTNER
+              </Heading>
+              <Text fontSize="xl" color="gray.400">
+                Give your restaurant and online business a boost
+              </Text>
+            </Box>
+            <Box py="4">
+              <Text>
+                We offer the best way for businesses such as yours to tap into a
+                big market online!
+              </Text>
+            </Box>
+            <Stack direction={{ base: "column", lg: "row" }}>
+              <Flex direction="column" justifyContent="space-evenly">
+                <SimpleGrid
+                  /*    templateRows="repeat(4, 1fr)"
               autoFlow="column"
               columns={{ base: 1, md: 2 }}
-              spacing={8}
-            >
-              {offers.map((item) => (
-                <HStack key={item} align="top">
-                  <Box color="yellow" px={2}>
-                    <Icon as={CheckIcon} />
-                  </Box>
-                  <VStack align="start">
-                    <Text>{item}</Text>
-                  </VStack>
-                </HStack>
-              ))}
-            </SimpleGrid>
-            <GetNotified />
+              spacing={8} */
+                  justifyContent="center"
+                  columns={{ base: 1, sm: 2, md: 2 }}
+                  spacing={8}
+                >
+                  {offers.map((item) => (
+                    <HStack key={item} align="top">
+                      <Box color="yellow" px={2}>
+                        <Icon as={CheckIcon} />
+                      </Box>
+                      <VStack align="start">
+                        <Text>{item}</Text>
+                      </VStack>
+                    </HStack>
+                  ))}
+                </SimpleGrid>
+                <GetNotified />
+              </Flex>
+            </Stack>
           </Flex>
-          <Box>
+          <Center>
             <PartnerImg />
-          </Box>
+          </Center>
         </Stack>
       </Container>
     </Section>
