94d316bc285798c6d7547d92a6e02f343e916d5d
icyjms <icyjms@yahoo.com>
2021-08-02 14:38:32 +0800

    Adjust the border color of checkbox

--- src/components/FormCheckbox.js
+++ src/components/FormCheckbox.js
@@ -5,7 +5,12 @@ const FormItem = ({ name, ...rest }) => {
   const [field, meta] = useField({ name, ...rest });
   return (
     <FormControl pb={[2, 4, 6]} isInvalid={meta.touched && meta.error}>
-      <Checkbox {...field} {...rest} isChecked={field.value} />
+      <Checkbox
+        {...field}
+        {...rest}
+        isChecked={field.value}
+        borderColor="gray.900"
+      />
       {meta.touched && meta.error && (
         <FormErrorMessage mt="1" fontSize="xs" {...{ type: 'error' }}>
           {meta.error}
